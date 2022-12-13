ec05c6e38c8d093b608367628bc871041efa0834
Kalvium <program@kalvieducation.com>
2022-12-13 23:31:24 +0530

    initial commit

---
+++ src/data/CarouselData.js
@@ -0,0 +1,17 @@
+import faces from "../Images/faces.jpg";
+import girlface from "../Images/girlface.jpg";
+import eyes from "../Images/eyes.jpg";
+
+export const images = [
+  { title: "Faces", subtitle: "How many faces do you have?", img: faces },
+  {
+    title: "Inside out",
+    subtitle: "The best person to guide you is yourself",
+    img: girlface,
+  },
+  {
+    title: "Crying Lady",
+    subtitle: "Why so serious?",
+    img: eyes,
+  },
+];
